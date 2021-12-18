2c715356075ee53c5d72f4e131765fb011d97155
tri218138 <80521604+tri218138@users.noreply.github.com>
2021-12-18 21:21:05 +0700

    Create main.cpp

---
+++ Introductory Problems/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi	vector<int>
+#define vll	vector<ll>
+#define	rep(i,a,b) for(int i = a; i < (int) b; i++)
+#define	repi(i,a,b) for(int i = a; i <= (int) b; i++)
+#define ff first
+#define ss second
+#define pii	pair<int,int>
+#define pci	pair<char,int>
+const int MOD = 1e9 + 7;
+int main() {
+
+	return 0;
+}
